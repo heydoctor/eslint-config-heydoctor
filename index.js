a9ee7f9766bbbc9e9bcce491a9d20628e22060b1
Kyle Alwyn <kjalwyn@gmail.com>
2018-02-12 16:26:39 -0800

    repo.init()

---
+++ index.js
@@ -0,0 +1,64 @@
+module.exports = {
+  "extends": [
+    "plugin:prettier/recommended",
+    "airbnb-base"
+  ],
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "ecmaVersion": 8,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "modules": true
+    }
+  },
+  "env": {
+    "es6": true,
+    "node": true,
+    "jest/globals": true
+  },
+  "plugins": [
+    "babel",
+    "import",
+    "security",
+    "jest"
+  ],
+  "rules": {
+    "no-use-before-define": 0,
+    "no-empty": 0,
+    "no-restricted-syntax": 0,
+    "no-await-in-loop": 0,
+    "camelcase": 0,
+    "arrow-parens": [
+      "error",
+      "as-needed"
+    ],
+    "no-confusing-arrow": [
+      "error",
+      {
+        "allowParens": true
+      }
+    ],
+
+    // Configure import
+    "import/prefer-default-export": 0,
+    "import/no-extraneous-dependencies": 0,
+
+    // Configure babel
+    "babel/new-cap": 1,
+
+    // Configure Jest
+    "jest/no-identical-title": "error",
+
+    // Configure Prettier
+    "prettier/prettier": ["error", {
+      "parser": "babylon",
+      "printWidth": 120,
+      "semi": true,
+      "useTabs": false,
+      "singleQuote": true,
+      "trailingComma": "es5",
+      "bracketSpacing": true,
+      "jsxBracketSameLine": true
+    }]
+  }
+}
---
+++ react.js
@@ -0,0 +1,31 @@
+module.exports = {
+  "extends": [
+    "airbnb"
+  ],
+  "env": {
+    "browser": true
+  },
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true,
+    }
+  },
+  "rules": {
+    "react/jsx-filename-extension": 0,
+    "react/require-default-props": 0,
+    "react/sort-comp": 0,
+    "react/prop-types": 0,
+    "react/jsx-closing-bracket-location": 0,
+    "jsx-a11y/anchor-is-valid": 0
+  },
+  "globals": {
+    "window": true,
+    "document": true
+  },
+  "settings": {
+    "import/resolver": {
+      "babel-module": {
+      }
+    }
+  }
+}
